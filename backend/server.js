0f79bc632fe796ddbe22386d0ab2dc8f2e563780
Riwaj-Thapa <rambathapa565@.com>
2025-01-23 17:02:43 +0545

    updating the server.js after user registration and role based access

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 
 import connectDB from "./config/db.js";
 
+import userRoutes from "./routes/userRoutes.js";
+
 dotenv.config();
 connectDB();
 
@@ -11,6 +13,8 @@ const app = express();
 // Middleware
 app.use(express.json());
 
+// Routes
+app.use("/api/users", userRoutes); // User routes for admin and employee
 
 // Start the server
 const PORT = process.env.PORT || 5000;
