a4bac4ea784fcf507afdc0c9bfb074320054b93f
Riwaj-Thapa <rambathapa562@gmail.com>
2025-09-02 13:11:29 +0545

    image error fixed

--- frontend/src/pages/admin/InventoryAdmin.jsx
+++ frontend/src/pages/admin/InventoryAdmin.jsx
@@ -206,7 +206,7 @@ const InventoryAdmin = () => {
       </div>
 
       <div className="bg-white rounded-lg shadow overflow-hidden">
-        <table className="w-[80%] divide-y divide-gray-200 mx-auto">
+        <table className="min-w-full divide-y divide-gray-200">
           <thead className="bg-gray-50">
             <tr>
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
